57ef904f9c0b93aea7c44c802ac2b792c53d7245
DIR\marco.accardi <accardi.marco@gmail.com>
2018-07-19 17:11:14 +0200

    test v2 matcher

--- src/main/java/com/example/selenium/matcher/MobileCapabilityMatcher.java
+++ src/main/java/com/example/selenium/matcher/MobileCapabilityMatcher.java
@@ -9,55 +9,45 @@ public class MobileCapabilityMatcher extends DefaultCapabilityMatcher {
 	private static final Logger LOG = Logger.getLogger(MobileCapabilityMatcher.class.getName());
 	private static String DEVICE_NAME = "deviceName";
 	private static String PLATFORM_VERSION = "platformVersion";
-	private static String VERSION = "version";
-
+	private static String PLATFORM_NAME = "platformName";
+	
+	public MobileCapabilityMatcher() {
+        super();
+   
+        
+    }
+	
 	@Override
 	public boolean matches(Map<String, Object> nodeCapability, Map<String, Object> requestedCapability) {
+		
+
 		if (!requestedCapability.containsKey(DEVICE_NAME)) {
 			LOG.info("Request received don't contains deviceName! It isn't a mobile request");
 			boolean basicChecks = super.matches(nodeCapability, requestedCapability);
 			LOG.info("basicChecks: " + basicChecks);
 			return (basicChecks);
 		} else {
+		     // Appium specific considerations
+	        this.addToConsider(PLATFORM_NAME);
+	        this.addToConsider(PLATFORM_VERSION);
+	        this.addToConsider(DEVICE_NAME);
+	        
 			LOG.info("Mobile request received! deviceName capability it's present.");
-			LOG.info("************************************************************************************");
-			LOG.info("NODE CAPABILITY:" + nodeCapability.size());
+			String listOfNodeCap="";
+			String listOfReqCap="";
 			
-			LOG.info("######### END");
-
-			LOG.info("REQUESTED CAPABILITY:" + requestedCapability.size());
-			
-			LOG.info("######### END");
-			LOG.info("************************************************************************************");
-			
-			String platformRequested = "";
-			LOG.info("Check the match with this node:" + nodeCapability.get(DEVICE_NAME) + "|"+ nodeCapability.get(PLATFORM_VERSION));
-			String deviceRequested = String.valueOf(requestedCapability.get(DEVICE_NAME));
-			
-			
-			if(requestedCapability.get(PLATFORM_VERSION)!=null && !String.valueOf(requestedCapability.get(PLATFORM_VERSION)).isEmpty()){
-				platformRequested = String.valueOf(requestedCapability.get(PLATFORM_VERSION));
-			}else{
-				platformRequested = String.valueOf(requestedCapability.get(VERSION));	
+			for (String key : nodeCapability.keySet()) {
+				listOfNodeCap=listOfNodeCap.concat(" | "+nodeCapability.get(key));
 			}
-			LOG.info("Requested capabilities:" + deviceRequested + "|" + platformRequested);
-
+			LOG.info("NODE CAPABILITY:" + listOfNodeCap);
 			
-			String platformNode="";
-			if(nodeCapability.get(PLATFORM_VERSION)!=null && !String.valueOf(nodeCapability.get(PLATFORM_VERSION)).isEmpty()){
-				platformNode = String.valueOf(nodeCapability.get(PLATFORM_VERSION));
-			}else{
-				platformNode = String.valueOf(nodeCapability.get(VERSION));	
+			for (String key : requestedCapability.keySet()) {
+				listOfReqCap=listOfReqCap.concat(" | "+requestedCapability.get(key));
 			}
-			String deviceNameNode=String.valueOf(nodeCapability.get(DEVICE_NAME));
-			LOG.info("Node capabilities:" + deviceNameNode + "|" + platformNode);
-			
-			
-			boolean isDeviceCorrect =deviceRequested.equalsIgnoreCase(deviceNameNode);
-			boolean isPlatformVersionCorrect = platformRequested.equalsIgnoreCase(platformNode);
-			LOG.info("isDeviceCorrect:" + isDeviceCorrect + " | isPlatformVersionCorrect:" + isPlatformVersionCorrect);
-			return isDeviceCorrect && isPlatformVersionCorrect;
-
+			LOG.info("REQUESTED CAPABILITY:" + listOfReqCap);
+			boolean advancedCheck = super.matches(nodeCapability, requestedCapability);
+			LOG.info("RESULT CHECK: "+advancedCheck);
+			return(advancedCheck);
 		}
 	}
 }
---
+++ src/main/java/com/example/selenium/matcher/MobileCapabilityMatcherV1.java
@@ -0,0 +1,63 @@
+package com.example.selenium.matcher;
+
+import org.openqa.grid.internal.utils.DefaultCapabilityMatcher;
+import java.util.logging.Logger;
+import java.util.Map;
+
+public class MobileCapabilityMatcherV1 extends DefaultCapabilityMatcher {
+
+	private static final Logger LOG = Logger.getLogger(MobileCapabilityMatcherV1.class.getName());
+	private static String DEVICE_NAME = "deviceName";
+	private static String PLATFORM_VERSION = "platformVersion";
+	private static String VERSION = "version";
+
+	@Override
+	public boolean matches(Map<String, Object> nodeCapability, Map<String, Object> requestedCapability) {
+		if (!requestedCapability.containsKey(DEVICE_NAME)) {
+			LOG.info("Request received don't contains deviceName! It isn't a mobile request");
+			boolean basicChecks = super.matches(nodeCapability, requestedCapability);
+			LOG.info("basicChecks: " + basicChecks);
+			return (basicChecks);
+		} else {
+			LOG.info("Mobile request received! deviceName capability it's present.");
+			LOG.info("************************************************************************************");
+			LOG.info("NODE CAPABILITY:" + nodeCapability.size());
+			
+			LOG.info("######### END");
+
+			LOG.info("REQUESTED CAPABILITY:" + requestedCapability.size());
+			
+			LOG.info("######### END");
+			LOG.info("************************************************************************************");
+			
+			String platformRequested = "";
+			LOG.info("Check the match with this node:" + nodeCapability.get(DEVICE_NAME) + "|"+ nodeCapability.get(PLATFORM_VERSION));
+			String deviceRequested = String.valueOf(requestedCapability.get(DEVICE_NAME));
+			
+			
+			if(requestedCapability.get(PLATFORM_VERSION)!=null && !String.valueOf(requestedCapability.get(PLATFORM_VERSION)).isEmpty()){
+				platformRequested = String.valueOf(requestedCapability.get(PLATFORM_VERSION));
+			}else{
+				platformRequested = String.valueOf(requestedCapability.get(VERSION));	
+			}
+			LOG.info("Requested capabilities:" + deviceRequested + "|" + platformRequested);
+
+			
+			String platformNode="";
+			if(nodeCapability.get(PLATFORM_VERSION)!=null && !String.valueOf(nodeCapability.get(PLATFORM_VERSION)).isEmpty()){
+				platformNode = String.valueOf(nodeCapability.get(PLATFORM_VERSION));
+			}else{
+				platformNode = String.valueOf(nodeCapability.get(VERSION));	
+			}
+			String deviceNameNode=String.valueOf(nodeCapability.get(DEVICE_NAME));
+			LOG.info("Node capabilities:" + deviceNameNode + "|" + platformNode);
+			
+			
+			boolean isDeviceCorrect =deviceRequested.equalsIgnoreCase(deviceNameNode);
+			boolean isPlatformVersionCorrect = platformRequested.equalsIgnoreCase(platformNode);
+			LOG.info("isDeviceCorrect:" + isDeviceCorrect + " | isPlatformVersionCorrect:" + isPlatformVersionCorrect);
+			return isDeviceCorrect && isPlatformVersionCorrect;
+
+		}
+	}
+}
